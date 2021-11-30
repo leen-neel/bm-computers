bf2eae7e4e5731209d0499e0291049b4beb3b87d
leen-neel <and24903@gmail.com>
2021-11-30 17:05:01 +0530

    added hero section

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
   <router-view />
 </template>
 <script>
-import { defineComponent } from 'vue';
-
+import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
 export default defineComponent({
-  name: 'App'
-})
+  name: "App",
+  setup() {
+    const $q = useQuasar();
+    $q.dark.set(true);
+  },
+});
 </script>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- Rendered on desktop -->
+  <div class="full-screen bg-dark row q-pa-xl">
+    <div class="col-6 heroPos">
+      <q-img
+        src="/images/cool.png"
+        spinner-color="primary"
+        spinner-size="82px"
+        style="border-radius: 16px"
+      />
+      <!-- <p class="text-h6 q-mt-sm">We manage hardware, software and more....</p> -->
+      <q-btn
+        type="a"
+        color="primary"
+        no-caps
+        class="cta q-mt-lg"
+        label="Learn more"
+        icon-right="arrow_forward"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HeroSection",
+});
+</script>
+
+<style lang="scss" scoped>
+.heroPos {
+  position: absolute;
+  left: 5%;
+  top: 30%;
+}
+
+.cta {
+  padding: 16px 40px;
+  border-radius: 8px;
+  font-size: 18px;
+  font-weight: 700;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  max-height: 56px;
+  max-width: 273px;
+}
+</style>
