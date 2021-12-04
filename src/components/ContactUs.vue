29d67aa0dddfad66e81de99c679ee2b06a87578c
leen-neel <and24903@gmail.com>
2021-12-04 21:04:38 +0530

    added blur to dialog bg

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -5,23 +5,23 @@
         <div class="text-h3 text-center poppins q-mb-md">Contact us</div>
       </div>
 
-      <div class="col-12 q-gutter-sm">
+      <div class="q-gutter-sm">
         <div class="q-ma-md flex-center flex q-gutter-sm">
-          <q-card dark>
+          <q-card class="col-4">
             <q-card-section class="flex flex-center">
               <q-icon name="call" size="40px" />
             </q-card-section>
             <q-card-section> +961-4-870-115 </q-card-section>
           </q-card>
 
-          <q-card class="my-card">
+          <q-card class="col-4">
             <q-card-section class="flex flex-center">
               <q-icon name="place" size="40px" />
             </q-card-section>
             <q-card-section> Beit Mery - Lebanon </q-card-section>
           </q-card>
 
-          <q-card class="my-card">
+          <q-card class="col-4">
             <q-card-section class="flex flex-center">
               <q-icon name="email" size="40px" />
             </q-card-section>
