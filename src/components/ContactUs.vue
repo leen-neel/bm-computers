d10f0d2336c80c173d480dd9b631fe1cea112ed8
leen-neel <and24903@gmail.com>
2021-12-06 12:39:24 +0530

    added email regex to contact form

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -122,7 +122,16 @@ export default defineComponent({
     const isLoading = ref(false);
 
     const disabled = computed(() => {
-      return name.value && email.value && subject.value && message.value;
+      let re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      return !(
+        name.value &&
+        email.value &&
+        subject.value &&
+        message.value &&
+        re.test(email.value)
+      );
     });
 
     const sendMessage = () => {
