46a3a0adcf858444f78dcc9147ec4f144660b867
leen-neel <and24903@gmail.com>
2021-12-10 17:43:19 +0530

    added layers

--- src/components/ProductsSlider.vue
+++ src/components/ProductsSlider.vue
@@ -1,76 +1,74 @@
 <template>
-  <div>
-    <q-carousel
-      v-if="$q.screen.gt.sm"
-      v-model="slider"
-      transition-prev="slide-right"
-      transition-next="slide-left"
-      swipeable
-      animated
-      padding
-      autoplay
-      infinite
-      height="30vh"
-      width="100%"
-      class="shadow-2 rounded-borders"
-    >
-      <q-carousel-slide :name="1" class="column no-wrap">
-        <div
-          class="
-            row
-            fit
-            full-height
-            items-center
-            q-col-gutter
-            no-wrap
-            flex-center
-          "
-        >
-          <q-img
-            class="q-mx-xl col-1"
-            src="/images/products/logitech_mouse.png"
-          />
+  <q-carousel
+    v-if="$q.screen.gt.sm"
+    v-model="slider"
+    transition-prev="slide-right"
+    transition-next="slide-left"
+    swipeable
+    animated
+    padding
+    autoplay
+    infinite
+    height="30vh"
+    width="100%"
+    class="shadow-2 rounded-borders bgc"
+  >
+    <q-carousel-slide :name="1" class="column no-wrap">
+      <div
+        class="
+          row
+          fit
+          full-height
+          items-center
+          q-col-gutter
+          no-wrap
+          flex-center
+        "
+      >
+        <q-img
+          class="q-mx-xl col-1"
+          src="/images/products/logitech_mouse.png"
+        />
 
-          <q-img class="q-mx-xl col-2" src="/images/products/samsung_ssd.png" />
+        <q-img class="q-mx-xl col-2" src="/images/products/samsung_ssd.png" />
 
-          <q-img
-            class="q-mx-xl col-1"
-            src="/images/products/invo_batteries.png"
-          />
+        <q-img
+          class="q-mx-xl col-1"
+          src="/images/products/invo_batteries.png"
+        />
 
-          <q-img
-            class="q-mx-xl col-2"
-            width="250px"
-            src="/images/products/thinkpad.png"
-          />
-        </div>
-      </q-carousel-slide>
-      <q-carousel-slide :name="2" class="column no-wrap">
-        <div
-          class="
-            row
-            fit
-            full-height
-            items-center
-            q-col-gutter
-            no-wrap
-            flex-center
-          "
-        >
-          <q-img class="q-mx-xl col-2" src="/images/products/repair.png" />
+        <q-img
+          class="q-mx-xl col-2"
+          width="250px"
+          src="/images/products/thinkpad.png"
+        />
+      </div>
+    </q-carousel-slide>
+    <q-carousel-slide :name="2" class="column no-wrap">
+      <div
+        class="
+          row
+          fit
+          full-height
+          items-center
+          q-col-gutter
+          no-wrap
+          flex-center
+        "
+      >
+        <q-img class="q-mx-xl col-2" src="/images/products/repair.png" />
 
-          <q-img
-            class="q-mx-xl col-2"
-            src="/images/products/computer-repair.png"
-          />
+        <q-img
+          class="q-mx-xl col-2"
+          src="/images/products/computer-repair.png"
+        />
 
-          <q-img class="q-mx-xl col-1" src="/images/products/router.png" />
+        <q-img class="q-mx-xl col-1" src="/images/products/router.png" />
 
-          <q-img class="q-mx-xl col-1" src="/images/products/ups.png" />
-        </div>
-      </q-carousel-slide>
-    </q-carousel>
-  </div>
+        <q-img class="q-mx-xl col-1" src="/images/products/ups.png" />
+      </div>
+    </q-carousel-slide>
+  </q-carousel>
 </template>
 
 <script>
@@ -84,3 +82,9 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.bgc {
+  background-color: #81a1c1;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,10 @@
   <q-page>
     <HeroSection />
 
+    <div class="spacer layer q-mt-xl"></div>
+    <ProductsSlider />
+    <div class="spacer rotate-180 layer q-mb-xl"></div>
+
     <AboutUs />
 
     <ContactUs />
@@ -12,6 +16,7 @@
 import AboutUs from "src/components/AboutUs.vue";
 import ContactUs from "src/components/ContactUs.vue";
 import HeroSection from "src/components/HeroSection.vue";
+import ProductsSlider from "src/components/ProductsSlider.vue";
 
 import { defineComponent } from "vue";
 
@@ -21,6 +26,7 @@ export default defineComponent({
     HeroSection,
     AboutUs,
     ContactUs,
+    ProductsSlider,
   },
 });
 </script>
